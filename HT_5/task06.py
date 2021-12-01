2a90ccf646690913760c75a941d8b54fbd89aa03
Vasya <prus.vasyl1619@vu.cdu.edu.ua>
2021-12-01 13:20:31 +0200

    реализовал возможность использовать отрицательный шаг

--- HT_5/task06.py
+++ HT_5/task06.py
@@ -1,16 +1,25 @@
 #Всі ви знаєте таку функцію як <range>. Напишіть свою реалізацію цієї функції.
 
 
-def my_range(stop, start = 0, step = 1):
-    if stop <= start:
-        raise Exception("початок повинен бути менший за зупинку")
+def my_range(start, stop, step = 1):
     i = start
+    if step < 0 or step == 0:
+        raise "Крок не повинен бути відємним числом і не дорівнювати нулю"
+    if i > stop:
+        while i > stop:
+            yield i
+            i -= step
+    
     while i < stop:
         yield i
         i += step
-
-
-for k in my_range(10):
-    print(k)
+    
+try:
+    for k in my_range(20, -8, 1):
+        print(k)
+except RuntimeError as ex:
+    print(ex)
+except:
+    print("Сталася помилка")
 
 
