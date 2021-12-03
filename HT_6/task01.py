8da7d421c00cc6e202796b2300443a1f204cef90
Vasya <prus.vasyl1619@vu.cdu.edu.ua>
2021-12-03 18:23:40 +0200

    HT_6

---
+++ HT_6/task01.py
@@ -0,0 +1,24 @@
+import time
+
+
+def traffic_lights():
+    
+    light_cars = ['Red', 'Yellow', 'Green']
+    
+    while True:
+        for i in range(4):
+            print(light_cars[0], light_cars[2])
+            time.sleep(1)
+        for k in range(2):
+            print(light_cars[1], light_cars[2])
+            time.sleep(1)
+        for j in range(4):
+            print(light_cars[2], light_cars[0])
+            time.sleep(1)
+        for x in range(2):
+            print(light_cars[1], light_cars[0])
+            time.sleep(1)
+        continue
+
+
+traffic_lights()
